31954abb01108d77fa3b2613b286b6ffa42bfcad
Ryan Ricard <ryan.ricard@gmail.com>
2011-09-29 16:40:45 -0500

    added comments to application.js

--- assets/js/application.js
+++ assets/js/application.js
@@ -2,6 +2,7 @@ $(function(){
 
 	var ApplicationRouter = Backbone.Router.extend({
 
+		//map url routes to contained methods
 		routes: {
 			"": "home",
 			"home": "home",
@@ -10,20 +11,26 @@ $(function(){
 		},
 
 		deselectPills: function(){
+			//deselect all navigation pills
 			$('ul.pills li').removeClass('active');
 		},
 
 		selectPill: function(pill){
+			//deselect all navigation pills
 			this.deselectPills();
+			//select passed navigation pill by selector
 			$(pill).addClass('active');
 		},
 
 		hidePages: function(){
+			//hide all pages with 'pages' class
 			$('div.pages').hide();
 		},
 
 		showPage: function(page){
+			//hide all pages
 			this.hidePages();
+			//show passed page by selector
 			$(page).show();
 		},
 
@@ -46,35 +53,44 @@ $(function(){
 
 	var ApplicationView = Backbone.View.extend({
 
+		//bind view to body element (all views should be bound to DOM elements)
 		el: $('body'),
 
+		//observe navigation click events and map to contained methods
 		events: {
 			'click ul.pills li.home-pill a': 'displayHome',
 			'click ul.pills li.about-pill a': 'displayAbout',
 			'click ul.pills li.contact-pill a': 'displayContact'
 		},
 
+		//called on instantiation
 		initialize: function(){
+			//set dependency on ApplicationRouter
 			this.router = new ApplicationRouter();
+
+			//call to begin monitoring uri and route changes
+			Backbone.history.start();
 		},
 
 		displayHome: function(){
+			//update url and pass true to execute route method
 			this.router.navigate("home", true);
 		},
 
 		displayAbout: function(){
+			//update url and pass true to execute route method
 			this.router.navigate("about", true);
 		},
 
 		displayContact: function(){
+			//update url and pass true to execute route method
 			this.router.navigate("contact", true);
 		}
 
 	});
 
+	//load application
 	new ApplicationView();
-	
-	Backbone.history.start();
 
 });
 
